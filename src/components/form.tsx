84c305f7c550b02039e7a4f8ac29032b8eb8736e
erixweb <erikmorenodev@gmail.com>
2024-07-20 13:52:16 +0200

    Set output as server

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -4,33 +4,37 @@ import { useState } from "preact/hooks"
 export default function Form() {
 	const [file, setFile] = useState<File | null>(null)
 
-    async function handleSubmit (event: Event) {
-        event.preventDefault()
+	async function handleSubmit(event: Event) {
+		event.preventDefault()
 
-        if (file) {
-            const formData = new FormData()
+		if (file) {
+			const formData = new FormData()
+			formData.append("file", file)
 
-            formData.append("file", file)
+			console.log(formData.get("file"))
+            
+			const res = await fetch("/api/", {
+				method: "POST",
+				body: formData,
+			})
 
-            const res = await fetch("/api/", {
-                method: "POST",
-                body: formData
-            })
-            const json = await res.json()
-        }
-    }
 
-    async function handleInputChange (event: React.ChangeEvent<HTMLInputElement>) {
-        const uploadedFile = event?.target?.files ?? []
+			//const json = await res.json()
+		}
+	}
 
-        if (uploadedFile) {
-            setFile(uploadedFile)
-        }
-    }
+	async function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+		const uploadedFile = event?.target?.files[0] ?? []
+
+		if (uploadedFile) {
+			setFile(uploadedFile)
+            
+		}
+	}
 
 	return (
 		<form onSubmit={handleSubmit}>
-			<input type="file" name="file" accept=".csv" onChange={handleInputChange}/>
+			<input type="file" name="file" accept=".csv" onChange={handleInputChange} />
 
 			<button type="submit">Enviar</button>
 		</form>
--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -1,7 +1,19 @@
 import type { APIRoute } from "astro"
+import csvToJSON from "convert-csv-to-json"
 
-export const POST: APIRoute = ({ request }: { request: Request }): Response => {
+export const POST: APIRoute = async ({ request }: { request: Request }) => {
+	const formData = await request.formData()
+	const file = formData.get("file")
 
-    console.log(request.body)
-    return new Response(JSON.stringify({ message: "" }))
+	if (!file) return new Response(JSON.stringify({ message: "Failed to upload file", status: 500 }))
+
+	if (file.type !== "text/csv") return new Response(JSON.stringify({ message: "Invalid mime type", status: 500 }))
+	
+	const fileContents = await file.text()
+
+	console.log(fileContents)
+
+	console.log(csvToJSON.fieldDelimiter(";").getJsonFromCsv(fileContents))
+
+	return new Response(JSON.stringify({ message: "" }))
 }
